2d5d9155686531ea57f74aa34d168ed8fa4eea20
mosamario <mario@mosa.at>
2015-10-10 14:23:31 +0200

    rfta

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -3,8 +3,14 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("Hello GitHub!");
+		
+		String text = "Hello GitHub, I am Developer";
+		int number = 0;
+				
+		System.out.print(text);
+		System.out.print(number);
+		System.out.print("!");
+		
 	}
 
 }
